1007f3b22132ab9b4ba838528ea3747deef4e5e4
cslhxac <cs.lhxac@gmail.com>
2013-12-02 01:10:42 -0600

    cpp

--- bnt_C/CreateFactorGraph.cpp
+++ bnt_C/CreateFactorGraph.cpp
@@ -1,16 +1,100 @@
 #include"FactorGraph.hpp"
 #include <mex.h>
 
+int get1DindexFromND(const mwSize nDimensions,const mwSize* d,const vector<int> indices){
+	int tmp = indices[nDimensions - 1];
+	for(int i = (nDimensions - 2);i >= 0;--i){
+		tmp = tmp * d[i] + indices[i];
+	}
+}
+vector<int> getNDindexFrom1D(const mwSize nDimensions,const mwSize* d,const int indices){
+	vector<int> indicesT;
+	indicesT.resize(nDimensions);
+	int tmp = indices;
+	for(int i = 0;i < nDimensions;++i){
+		indicesT[i] = tmp%d[i];
+		tmp /= d[i];
+	}
+	return indicesT;
+}
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	//prhs[0] is the factor matrix
+	//prhs[0] is the factor matrix cell array
 	//prhs[1] is the array of number of possible values
-	//prhs[2] is the list of node names(optional)
+	//Make prhs[0] and prhs[1] DOUBLE arrays!!!!
+	#define FACTORMATRIX prhs[0]
+	#define NODESNVALUES prhs[1]
+	//NODELIKELIHOOD is a cell array
+	#define NODELIKELIHOOD prhs[2]
+	//FACTORTENDENCY is a cell array
+	#define FACTORTENDENCY prhs[3]
+	//prhs[4] is the list of node names(optional)
 	const mwSize *dims0 = mxGetDimensions(prhs[0]);
 	const mwSize *dims1 = mxGetDimensions(prhs[1]);
+	int nFactors = mxGetNumberOfElements(prhs[0]);
+	vector<vector<double>> factorM;
+	vector<vector<int>> factorMt;
+	factorM.resize(nFactors);
+	factorMt.resize(nFactors);
+	for(int i = 0;i < nFactors;i++){
+		mwSize n = mxGetN(mxGetCell(prhs[0],i));
+		mwSize m = mxGetM(mxGetCell(prhs[0],i));
+		mwSize tsize = n*m;
+		factorM[i] = vector<double>((double*) mxGetPr(mxGetCell(prhs[0],i)),(double*) mxGetPr(mxGetCell(prhs[0],i)) + tsize);
+		factorMt[i].resize(factorM[i].size());
+		for(int j = 0;j < factorM[i].size();++j){
+			factorMt[i][j] = factorM[i][j];
+		}
+		//mexPrintf("Factor %i: %i,%i\n",i,mxGetN(mxGetCell(prhs[0],i)),mxGetM(mxGetCell(prhs[0],i)));
+		//assert(mxGetM(mxGetCell(prhs[0],i)) == 1);
+	}
+	mwSize n = mxGetN(prhs[1]);
+	mwSize m = mxGetM(prhs[1]);
+	mwSize tsize = n*m;
+	vector<double> nValues = vector<double>((double*) mxGetPr(prhs[1]),(double*) mxGetPr(prhs[1]) + tsize);
+	vector<int> nValuest;
+	nValuest.resize(nValues.size());
+	for(int i = 0;i < nValues.size();i++){
+		nValuest[i] = nValues[i];
+	}
+	//mexPrintf("n,m: %i,%i\n",n,m);
 
-	double* factorM = (double*) mxGetPr(prhs[0]);
-	double* nValues = (double*) mxGetPr(prhs[0]);
-
-	mexPrintf("Hello World!\n");
+	//mexPrintf("factorM,nValues: %i,%i\n",factorM.size(),nValues.size());
+	//assert(m == 1);
+	FactorGraph* FG = new FactorGraph(factorMt,nValuest);
+	FG -> print();
+	//Get the Likelihood
+	int nNodes = mxGetNumberOfElements(NODELIKELIHOOD);
+	vector<vector<double>> nodeLikelihoodV;
+	nodeLikelihoodV.resize(nNodes);
+	for(int i = 0;i < nNodes;i++){
+		mwSize n = mxGetN(mxGetCell(NODELIKELIHOOD,i));
+		mwSize m = mxGetM(mxGetCell(NODELIKELIHOOD,i));
+		mwSize tsize = n*m;
+		nodeLikelihoodV[i] = vector<double>((double*) mxGetPr(mxGetCell(NODELIKELIHOOD,i)),(double*) mxGetPr(mxGetCell(NODELIKELIHOOD,i)) + tsize);
+	}
+	FG -> setNodeLikelihood(nodeLikelihoodV);
+	//Get the Tendency
+	int nTendency = mxGetNumberOfElements(FACTORTENDENCY);
+	if(nTendency != nFactors){
+		mexPrintf("Tendency Factor size mismatch");
+		return;
+	}
+	for(int i = 0;i < nTendency;i++){
+		map<vector<int>,double> tendencyMap;
+		mwSize nDimensions = mxGetNumberOfDimensions(mxGetCell(FACTORTENDENCY,i));
+		const mwSize* d = mxGetDimensions(mxGetCell(FACTORTENDENCY,i));
+		if(nDimensions != FG -> getFactor(i) -> nodes.size()){
+			mexPrintf("Tendency Factor nodes size mismatch");
+			return;
+		}
+		int nElements = 1;
+		for(int j = 0;j < nDimensions;++j){
+			nElements *= d[j];
+		}
+		for(int j = 0;j < nElements;++j){
+			tendencyMap[getNDindexFrom1D(nDimensions,d,j)] = mxGetPr(mxGetCell(FACTORTENDENCY,i))[j];
+		}
+		FG -> setFactorTendency(i,tendencyMap);
+	}
 }
--- bnt_C/FactorGraph.hpp
+++ bnt_C/FactorGraph.hpp
@@ -1,68 +1,89 @@
 #include<vector>
+#include<map>
 #include<iostream>
 #include<cstring>
+#include <mex.h>
 #ifndef FACTORGRAPH
 #define FACTORGRAPH
 using namespace std;
 struct Node{
 	string name;
     int nValues;
+	bool isEvidence;
     vector<double> likelihood;
     Node(int n,string name_in = ""):nValues(n),name(name_in){
         likelihood.resize(n);
-		for(vector<double>::iterator i = likelihood.begin();i != likelihood.end();++i){
-			*i = 1;
+		for(int i = 0;i < n;++i){
+			likelihood[i] = 0;
 		}
     }
 };
 struct Factor{
-	int nNode;
-	vector<Node*> nodes;
-	vector<double> tendency;
-	Factor(std::vector<Node*> & nodes_in):nodes(nodes_in){
-		int N = 1;
-		for(vector<Node*>::iterator i = nodes.begin();i != nodes.end();++i){
-			N *= (*i) -> nValues;
-		}
+	vector<int> nodes;
+	map<vector<int>,double> tendency;
+	Factor(const vector<int> & nodes_in):nodes(nodes_in){
 	}
-	double getValue(const vector<int> values)const{
-		if(values.size != nodes.size){
+	double getValue(const vector<int> values){
+		if(values.size() != nodes.size()){
 			cerr << "Factor::getValue: index, node size mismatch!" << endl;
 			return 0;
 		}
-		int index = 0;
-		vector<Node*>::const_iterator i = nodes.begin();
-		vector<int>::const_iterator j = values.begin();
-		index = *j;
-		for(++j;j != values.end();++i,++j){
-			index = index * (*i) -> nValues + *j;
-		}
-		return tendency[index];
+		return tendency[values];
 	}
 	void setValue(const vector<int> values,double v){
-		if(values.size != nodes.size){
+		if(values.size() != nodes.size()){
 			cerr << "Factor::getValue: index, node size mismatch!" << endl;
 			return;
 		}
-		int index = 0;
-		vector<Node*>::const_iterator i = nodes.begin();
-		vector<int>::const_iterator j = values.begin();
-		index = *j;
-		for(++j;j != values.end();++i,++j){
-			index = index * (*i) -> nValues + *j;
-		}
-		tendency[index] = v;
+		tendency[values] = v;
 	}
 };
 class FactorGraph{
 public:
-	FactorGraph(int* factorM,int* nValues,int n,int nfactor){
-		nodes.resize(n);
-		for(int i = 0;i < n;++i){
-			nodes[i] = new Node(nValues[i]);
+	FactorGraph(const vector<vector<int>>& factorM,const vector<int>& nValues){
+		nodes.resize(nValues.size());
+		int itr;
+		for(itr = 0;itr < nValues.size();++itr){
+			nodes[itr] = new Node(nValues[itr]);
+		}
+		factors.resize(factorM.size());
+		for(itr = 0;itr < factorM.size();++itr){
+			factors[itr] = new Factor(factorM[itr]);
 		}
 	};
-	~FactorGraph(){};
+	~FactorGraph(){
+		for(int i = 0;i < nodes.size();++i){
+			delete nodes[i];
+		}
+		for(int i = 0;i < factors.size();++i){
+			delete factors[i];
+		}
+	};
+	void setEvidenceList(vector<int> evidences){
+		for(int i = 0;i < evidences.size();++i){
+			nodes[evidences[i]] -> isEvidence = true;
+		}
+	}
+	void setNodeLikelihood(const vector<vector<double>>& likelihood){
+		for(int i = 0;i < likelihood.size();++i){
+			nodes[i] -> likelihood = likelihood[i];
+		}
+	}
+	void setFactorTendency(int factorID,const map<vector<int>,double>& tendency){
+		factors[factorID] -> tendency = tendency;
+	}
+	const Node* getNode(const int index)const{return nodes[index];};
+	const Factor* getFactor(const int index)const{return factors[index];};
+	int getnFactors()const{return factors.size();};
+	int getnNodes()const{return nodes.size();};
+	const void print()const{
+		for(int i = 0;i < factors.size();++i){
+			mexPrintf("Factor %i:\n",i);
+			for(int j = 0;j < factors[i] -> nodes.size();++j){
+				mexPrintf("\tNode %i: %i\n",factors[i] -> nodes[j],nodes[factors[i] -> nodes[j]] -> nValues);
+			}
+		}
+	}
 private:
 	vector<Node*> nodes;
 	vector<Factor*> factors;
